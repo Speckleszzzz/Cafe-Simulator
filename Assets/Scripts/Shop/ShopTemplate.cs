336d780796964fe8c84da1ecfdb7e1bce8db9e48
akash-charles <speckleszzzz@gmail.com>
2024-02-29 23:27:09 +0530

    shop canvas half done

---
+++ Assets/Scripts/Shop/ShopTemplate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ShopTemplate : MonoBehaviour
+{
+   public TMP_Text titleTxt;
+   public TMP_Text descriptionTxt;
+   public TMP_Text CoinTxt;
+   public Image iconImage;
+}
