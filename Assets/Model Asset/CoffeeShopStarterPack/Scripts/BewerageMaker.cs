03ac02f72e0c23ff9455d88da3db7e4f986bb097
akash-charles <speckleszzzz@gmail.com>
2024-03-17 21:26:25 +0530

    1234

--- Assets/Model Asset/CoffeeShopStarterPack/Scripts/BewerageMaker.cs
+++ Assets/Model Asset/CoffeeShopStarterPack/Scripts/BewerageMaker.cs
@@ -1,104 +1,52 @@
-﻿// 
-// BewerageMaker.cs
-//
-// Used on interactable products like espresso machine, mocha pot and tea pot.
-//
-// Author:
-//       K.Sinan Acar <ksa@puzzledwizard.com>
-//
-// Copyright (c) 2019 PuzzledWizard
-// ******------------------------------------------------------******
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 namespace PW
 {
     [RequireComponent(typeof(Collider))]
     public class BewerageMaker : MonoBehaviour
     {
-        #region AnimationSettings
-        [SerializeField]
-        public bool useAnimation = true;
-
-        [SerializeField]
-        public bool useTweeningAnimation = false;
-
-        //Animation state for prefill.
+        [SerializeField] public bool useAnimation = true;
+        [SerializeField] public bool useTweeningAnimation = false;
         public string preFillAnimationStateName;
-
-        //time takes to prefill.
         public float preFillProcess;
-
-        //Animation ending state for fillEnded.
         public string fillEndedAnimationState;
-
-        //time takes to fill the cup with particle.
         public float fillingProcess;
+        [SerializeField] public Transform dummyAnimationTarget;
+        [SerializeField] public ParticleSystem fillParticle;
+        [SerializeField] public Transform finalTweenTarget;
 
-        [SerializeField]
-        public Transform dummyAnimationTarget;
-        //optional particle system to show drink getting filled.
-        [SerializeField]
-        public ParticleSystem fillParticle;
-
-        //Used with tweening animation, to figure out the end position and rotation of the object.
-        //You can duplicate the object, set it's transforms and delete the components like Mesh filter and renderer.
-        [SerializeField]
-        public Transform finalTweenTarget;
-
-        #endregion
-
-        //Drink prefab created from the product manager
         public GameObject cupType;
-
-        //UI indicator to show progress
         public GameObject progressHelperprefab;
-
-        //the position where the cup will be placed on instantiate
         public Transform fillCupSpot;
 
-
-        #region private variables
         private float totalProcess;
-
-        bool canFillCup = true;
-
-        FillCupHelper fillCupHelper;
-
-        ProgressHelper m_progressHelper;
-
-        Collider m_Collider;
-
-        Animator m_animator;
-        #endregion
+        private bool canFillCup = true;
+        private FillCupHelper fillCupHelper;
+        private ProgressHelper m_progressHelper;
+        private Collider m_Collider;
+        private Animator m_animator;
 
         void Start()
         {
-            //find total time to process
             totalProcess = preFillProcess + fillingProcess;
 
-            //Instantiate and set the UI indicator
             if (progressHelperprefab != null)
             {
                 m_progressHelper = Instantiate(progressHelperprefab, transform).GetComponent<ProgressHelper>();
-
                 m_progressHelper.ToggleHelper(false);
             }
 
-            //get the collider and enable it 
             m_Collider = GetComponent<Collider>();
-
             m_Collider.enabled = true;
 
-            //Get the animator from the dummy Target
             if (dummyAnimationTarget != null)
                 m_animator = dummyAnimationTarget.GetComponent<Animator>();
 
-            //if you want to use tweening we need to disable the animator.
-            //It would override the transforms if we didn't
             if (useTweeningAnimation)
             {
                 if(m_animator!=null)
@@ -107,9 +55,9 @@ namespace PW
                 
         }
 
-        void OnMouseUp()
+        void Update()
         {
-            if (canFillCup)
+            if (canFillCup && Input.GetKeyDown(KeyCode.E))
             {
                 StartFillingStep();
             }
@@ -118,15 +66,10 @@ namespace PW
         void StartFillingStep()
         {
             canFillCup = false;
-
-            //Instantiate and set the cup 
             SetTheCup();
-
-            //Show the indicator
             if(m_progressHelper!=null)
                 m_progressHelper.ToggleHelper(true);
 
-            //Start playing the animations or tweening
             if (!string.IsNullOrEmpty(preFillAnimationStateName) || useTweeningAnimation)
             {
                 StartPreFillAnimationState();
@@ -134,7 +77,6 @@ namespace PW
             else
             {
                 StartCoroutine(DoFillAnimation());
-
             }
         }
 
@@ -146,15 +88,12 @@ namespace PW
                 {
                     m_animator.SetTrigger(preFillAnimationStateName);
                 }
-
             }
-                StartCoroutine(DoPreFill(dummyAnimationTarget, finalTweenTarget));
+            StartCoroutine(DoPreFill(dummyAnimationTarget, finalTweenTarget));
         }
 
         IEnumerator DoPreFill(Transform target, Transform finalTweenValue)
         {
-            //We do the tweening and update UI in this coroutine.
-
             float curPreFill = preFillProcess;
             Vector3 totalDist = Vector3.zero;
             Vector3 totalRot = Vector3.zero;
@@ -177,103 +116,66 @@ namespace PW
                 }
 
                 curPreFill -= Time.deltaTime;
-
                 var now = preFillProcess - curPreFill;
                 m_progressHelper.UpdateProcessUI(now, totalProcess);
-
                 yield return null;
             }
-
-            //Starts Fill ended animation and the particle
             StartCoroutine(DoFillAnimation());
         }
 
         IEnumerator DoFillAnimation()
         {
-            //If we don't have animation to play just fill the cup
             if (!useTweeningAnimation && (string.IsNullOrEmpty(fillEndedAnimationState) || fillingProcess < 0.001f))
             {
                 fillCupHelper.DoFill(0f);
             }
             else
             {
-
                 fillCupHelper.DoFill(fillingProcess);
-
                 if (fillParticle != null)
                     fillParticle.Play();
                 float fillCurrent = fillingProcess;
 
                 while (fillCurrent > 0)
                 {
-
                     fillCurrent -= Time.deltaTime;
-
                     var valNow = preFillProcess + fillingProcess - fillCurrent;
-
                     if (m_progressHelper != null)
                         m_progressHelper.UpdateProcessUI(valNow, totalProcess);
-
                     yield return null;
                 }
-
             }
-
             OnFillEnded();
-
         }
-
-
 
         void OnFillEnded()
         {
-            //hide the UI indicator
             if(m_progressHelper!=null)
                 m_progressHelper.ToggleHelper(false);
 
-            //disable collider, because we don't want interaction on the object
             m_Collider.enabled = false;
-
-            //tell the cup to enable its collider and set necessary things 
             fillCupHelper.FillEnded();
-
             if (fillParticle != null)
-            {
                 fillParticle.Stop();
-            }
-
             StartCoroutine(DoFillEnded());
-
         }
 
-        
         void SetTheCup()
         {
             GameObject cup = Instantiate(cupType, fillCupSpot);
-
             fillCupHelper = cup.GetComponent<FillCupHelper>();
-
             fillCupHelper.SetMachine(this);
-
             if(m_progressHelper!=null)
                 m_progressHelper.ToggleHelper(true);
-
         }
 
         public void UnSetTheCup()
         {
             canFillCup = true;
-
             fillCupHelper = null;
-
             m_Collider.enabled = true;
         }
 
-        /// <summary>
-        /// Required things to do after fill ended such as reverse movement of a teapot,
-        /// or playing an animation.
-        /// </summary>
-        /// <returns></returns>
         IEnumerator DoFillEnded()
         {
             if (m_animator != null && !string.IsNullOrEmpty(fillEndedAnimationState))
@@ -284,8 +186,6 @@ namespace PW
             Vector3 FinalRotation;
             if (useTweeningAnimation)
             {
-                //make the reverse movement as we did in the prefill
-
                 float reverseMove = preFillProcess;
                 if (dummyAnimationTarget != null && finalTweenTarget != null)
                 {
@@ -294,15 +194,12 @@ namespace PW
                      totalDist = (FinalPosition - dummyAnimationTarget.transform.position);
                      totalRot = (FinalRotation - dummyAnimationTarget.transform.rotation.eulerAngles);
                 }
-
                 while (reverseMove > 0)
                 {
                     if (useTweeningAnimation)
                     {
                         dummyAnimationTarget.transform.position += (Time.deltaTime * totalDist) / preFillProcess;
                         dummyAnimationTarget.transform.rotation = Quaternion.Euler(dummyAnimationTarget.transform.rotation.eulerAngles + (Time.deltaTime * totalRot) / preFillProcess);
-                        
-
                     }
                     reverseMove -= Time.deltaTime;
                     yield return null;
@@ -312,11 +209,8 @@ namespace PW
                     dummyAnimationTarget.localPosition = Vector3.zero;
                     dummyAnimationTarget.transform.localRotation = Quaternion.identity;
                 }
-                
             }
-
             yield return null;
         }
-
     }
 }
